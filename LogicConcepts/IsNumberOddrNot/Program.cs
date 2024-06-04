3bb91aab141bed283b17c33b829f1104c8621f9f
CARLOS ARTURO LEDEZMA CHAVERRA <carlosledezma230316@correo.itm.edu.co>
2024-06-04 00:50:11 -0500

    Se crea codigo y proyecto numero par o no.

---
+++ LogicConcepts/IsNumberOddrNot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+var NumberString = String.Empty;
+
+do
+{ 
+    Console.WriteLine("Ingrese numero  entero para validar o presione 'S' para salir ");
+
+    NumberString = Console.ReadLine();
+
+    if(NumberString! .ToLower() == "s")
+    {
+        continue;
+    }
+
+    var NumberInt = 0;
+
+if  (int.TryParse(NumberString, out NumberInt))
+{
+        if ((NumberInt % 2 == 0))
+        {
+        }
+        else
+        {
+            Console.WriteLine($"El {NumberInt} no es par.");
+        }
+ }
+
+else
+    {
+ Console.WriteLine($" ¡El valor que ingresastes:{NumberString}, no es valido!");
+    }
+} while (NumberString != "s");
+
+Console.WriteLine("El programa ha finalizado");
